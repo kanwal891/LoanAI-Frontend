b03bb33dd0326d9f6f3d51e9aa16a451f9eccda4
Amrutha2204 <nallaamruthareddy1@gmail.com>
2026-08-18 14:06:48 +0530

    Logo added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,10 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/logo.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
   },
 }
 
